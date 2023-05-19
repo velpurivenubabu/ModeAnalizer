90ba75f1f88b2774ff2bf08e0ee53cb3a49d4b6c
venubabu <velpurivenubabu123@gmail.com>
2023-05-19 21:31:25 +0530

    [Venubabu] Done With Welcome Message

---
+++ ModeAnalizer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ModeAnalizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! Welcome To mode Analizer problem");
+        }
+    }
+}
