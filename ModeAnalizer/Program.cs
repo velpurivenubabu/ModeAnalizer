81447668e451767bcfbc0d8929c5e9eceba4f836
venubabu <velpurivenubabu123@gmail.com>
2023-05-22 22:03:53 +0530

    [Venubabu] Done With CustumExceptionUC3

--- ModeAnalizer/Program.cs
+++ ModeAnalizer/Program.cs
@@ -1,14 +1,19 @@
-﻿namespace ModeAnalizer
+﻿using MoodAnalyser;
+
+namespace ModeAnalizer
 {
 
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("mood analyser");
-            HappySad happySad = new HappySad();
-            string Result = happySad.AnalysingMood();
-            Console.WriteLine(Result);
+            static void Main(string[] args)
+            {
+                Console.WriteLine("mood analyser");
+                HappySad happySad = new HappySad();
+                string Result = happySad.AnalysingMood();
+                Console.WriteLine(Result);
+            }
         }
     }
 }
